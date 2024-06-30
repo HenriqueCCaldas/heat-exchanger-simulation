c11260043323391728daf8697237077af032ae52
HenriqueCCaldas <henrique.c.caldas@tum.de>
2024-06-30 22:05:16 +0200

    Added run 900 s at 0.05

--- shellTubeHEX/results/transient/generalDataAnalysis/scripts/heatTransferRatePlot.py
+++ shellTubeHEX/results/transient/generalDataAnalysis/scripts/heatTransferRatePlot.py
@@ -3,20 +3,20 @@ import matplotlib.pyplot as plt
 import math as math 
 
 # Read the CSV file into a pandas DataFrame
-data1 = pd.read_csv("/media/henrique/Elements/heatExchangerSimulation/shellTubeHEX/transient/results/generalDataAnalysis/data/outletResultsT1-1.csv")
-data2 = pd.read_csv("/media/henrique/Elements/heatExchangerSimulation/shellTubeHEX/transient/results/generalDataAnalysis/data/outletResultsT1-2.csv")
-data3 = pd.read_csv("/media/henrique/Elements/heatExchangerSimulation/shellTubeHEX/transient/results/generalDataAnalysis/data/outletResultsT1-3_900.csv")
+data1 = pd.read_csv("/media/henrique/Elements/heatExchangerSimulation/shellTubeHEX/transient/results/generalDataAnalysis/data/outletResults0-01_900.csv")
+data2 = pd.read_csv("/media/henrique/Elements/heatExchangerSimulation/shellTubeHEX/transient/results/generalDataAnalysis/data/outletResults0-03_900.csv")
+data3 = pd.read_csv("/media/henrique/Elements/heatExchangerSimulation/shellTubeHEX/transient/results/generalDataAnalysis/data/outletResults0-05_900.csv")
 
 # Extract the temperature column from the DataFrame
 temperature1 = data1["avg(T)"]
 temperature2 = data2["avg(T)"]
-temperature3 = data3["avg(T)"]
+temperature3 = data3["avg(T)"] 
 
 velocity1 = data1["avg(U (Magnitude))"]
 velocity2 = data2["avg(U (Magnitude))"]
 velocity3 = data3["avg(U (Magnitude))"]
 
-time300 = data1["Time"]
+#time300 = data1["Time"]
 time900 = data3["Time"]
 
 cp = 4179 # J/kg*K
@@ -29,8 +29,8 @@ Q3 = cp * density * (temperature3 * velocity3) * area
 
 
 plt.plot(time900, Q3, label='Ushell = 0.01(m/s)', color='red')
-plt.plot(time300, Q1, label='Ushell = 0.05 (m/s)', color='green')
-plt.plot(time300, Q2, label='Ushell = 0.1 (m/s)', color='blue')
+#plt.plot(time900, Q1, label='Ushell = 0.05 (m/s)', color='green')
+#plt.plot(time900, Q2, label='Ushell = 0.1 (m/s)', color='blue')
 
 
 plt.legend()
