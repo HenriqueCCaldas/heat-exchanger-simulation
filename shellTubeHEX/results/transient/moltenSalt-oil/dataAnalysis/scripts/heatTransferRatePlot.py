a8a772d1e8ff607e1109bc8150ff3428dd7868c2
HenriqueCCaldas <henrique.c.caldas@tum.de>
2024-07-09 16:59:42 +0200

    Adding new plots molten salt

--- shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/scripts/heatTransferRatePlot.py
+++ shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/scripts/heatTransferRatePlot.py
@@ -5,12 +5,21 @@ from matplotlib.ticker import ScalarFormatter
 
 # Read the CSV file into a pandas DataFrame
 
-data1 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil.csv")
+data1 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-01.csv")
+data1v2 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-01NoRes.csv")
+data2 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-079479.csv")
+data3 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-005.csv")
 
 # Extract the temperature column from the DataFrame
 temperature1 = data1["avg(T)"]
+temperature1v2 = data1v2["avg(T)"]
+temperature2 = data2["avg(T)"]
+temperature3 = data3["avg(T)"]
 
 velocity1 = data1["avg(U (Magnitude))"]
+velocity1v2 = data1v2["avg(U (Magnitude))"]
+velocity2 = data2["avg(U (Magnitude))"]
+velocity3 = data3["avg(U (Magnitude))"]
 
 time900 = data1["Time"]
 
@@ -19,10 +28,16 @@ density = 2263.723 - (0.636 * temperature1)  # kg/m^3
 area = 0.04 * 0.04 * math.pi  # m^2
 initialTemperature =  563.15 # K
 
-print(temperature1)
-Q1 = cp * density * ((temperature1 - initialTemperature) * velocity1) * area * 1/1000 # W to kW
 
-plt.plot(time900, Q1, label='Ushell = 0.01(m/s), Re=1000', color='red')
+Q1 = cp * density * ((temperature1 - initialTemperature) * velocity1) * area * 1/1000 # W to kW
+Q1v2 = cp * density * ((temperature1v2 - initialTemperature) * velocity1v2) * area * 1/1000 # W to kW
+Q2 = cp * density * ((temperature2 - initialTemperature) * velocity2) * area * 1/1000 # W to kW
+Q3 = cp * density * ((temperature3 - initialTemperature) * velocity3) * area * 1/1000 # W to kW
+
+plt.plot(time900, Q3, label='Ushell = 0.005(m/s)', color='black')
+plt.plot(time900, Q1, label='Ushell = 0.01(m/s)', color='red')
+#plt.plot(time900, Q1v2, label='Ushell = 0.01(m/s) - No Resistance', color='green')
+plt.plot(time900, Q2, label='Ushell = 0.079479(m/s)', color='blue')
 
 plt.legend()
 plt.xlabel('Time (s)')
@@ -32,6 +47,6 @@ plt.xticks(range(0, int(max(time900)) + 100, 100))
 
 plt.grid(True)
 
-fileName ="heatTransferRate"
+fileName ="heatTransferRatePlotGeneral"
 plt.savefig('/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/plots/' + fileName + '.png')
 plt.show()
--- shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/scripts/temperaturePlot.py
+++ shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/scripts/temperaturePlot.py
@@ -1,44 +1,35 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data1 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/water-water/dataAnalysis/data/baffles/outletResultsBaffleRe1000.csv")
-data2 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/water-water/dataAnalysis/data/baffles/outletResultsBaffleRe3000.csv")
-data3 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/water-water/dataAnalysis/data/baffles/outletResultsBaffleRe5000.csv")
-
-data4 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/water-water/dataAnalysis/data/noBaffles/outletResultsNoBaffleRe1000.csv")
-data5 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/water-water/dataAnalysis/data/noBaffles/outletResultsNoBaffleRe3000.csv")
-data6 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/water-water/dataAnalysis/data/noBaffles/outletResultsNoBaffleRe5000.csv")
-
+data1 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-01.csv")
+data1v2 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-01NoRes.csv")
+data2 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-079479.csv")
+data3 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-005.csv")
 # Extract the temperature column from the DataFrame
 temperature1 = data1["avg(T)"]
+temperature1v2 = data1v2["avg(T)"]
 temperature2 = data2["avg(T)"]
-temperature3 = data3["avg(T)"]
-temperature4 = data4["avg(T)"]
-temperature5 = data5["avg(T)"]
-temperature6 = data6["avg(T)"]
+temperarture3 = data3["avg(T)"]
 
 # Plot the temperature data
 plt.grid(True) 
 
-time900 = data3["Time"]
-time900NoBaffles = data4["Time"]
+time900 = data1["Time"]
 
 # Choose the data to plot (Please select pairs 1-4, 2-5, 3-6)
 
-#plt.plot(time900, temperature1, label='Ushell = 0.01(m/s), Re = 1000', color='red')
-#plt.plot(time900, temperature2, label='Ushell = 0.03 (m/s), Re = 3000', color='green')
-plt.plot(time900, temperature3, label='Ushell = 0.05 (m/s), Re = 5000', color='blue')
-#plt.plot(time900NoBaffles, temperature4, label='Ushell = 0.01(m/s), Re = 1000, No Baffles', color='blue')
-#plt.plot(time900, temperature5, label='Ushell = 0.03 (m/s), Re = 3000, No Baffles', color='purple')
-plt.plot(time900, temperature6, label='Ushell = 0.05 (m/s), Re = 5000, No Baffles', color='orange')
+plt.plot(time900, temperature1, label='Ushell = 0.01(m/s)', color='red')
+#plt.plot(time900, temperature1v2, label='Ushell = 0.01 (m/s) - No Resistance', color='green')
+plt.plot(time900, temperature2, label='Ushell = 0.079479 (m/s)', color='blue')
+plt.plot(time900, temperarture3, label='Ushell = 0.005 (m/s)', color='black')
 
 plt.legend()
 plt.xlabel('Time (s)')
 plt.ylabel('Temperature (K)')
-plt.title('Outlet Average Temperature')
+plt.title('Outlet Average Temperature - Molten Salt & Thermal Oil')
 plt.xticks(range(0, int(max(time900)) + 100, 100))
-
-plt.savefig('temperaturePlot0-05.png')
+fileName ="TemperaturePlotGerenal"
+plt.savefig('/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/plots/' + fileName + '.png')
 plt.show()
 
 
