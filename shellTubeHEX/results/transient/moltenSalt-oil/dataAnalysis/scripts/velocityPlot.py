fdbf8ac12a9374c76166a1cd5df948b0a75f0a9c
HenriqueCCaldas <henrique.c.caldas@tum.de>
2024-07-09 17:28:00 +0200

    Fixing commit

--- shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/scripts/velocityPlot.py
+++ shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/scripts/velocityPlot.py
@@ -3,34 +3,25 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import ScalarFormatter
 
 # Read the CSV file into a pandas DataFrame
-data1 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/water-water/dataAnalysis/data/baffles/outletResultsBaffleRe1000.csv")
-data2 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/water-water/dataAnalysis/data/baffles/outletResultsBaffleRe3000.csv")
-data3 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/water-water/dataAnalysis/data/baffles/outletResultsBaffleRe5000.csv")
-
-data4 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/water-water/dataAnalysis/data/noBaffles/outletResultsNoBaffleRe1000.csv")
-data5 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/water-water/dataAnalysis/data/noBaffles/outletResultsNoBaffleRe3000.csv")
-data6 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/water-water/dataAnalysis/data/noBaffles/outletResultsNoBaffleRe5000.csv")
+data1 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-01.csv")
+data1v2 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-01NoRes.csv")
+data2 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-079479.csv")
+data3 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-005.csv")
 
 # Extract the temperature column from the DataFrame
 velocity1 = data1["avg(U (Magnitude))"]
+velocity1v2 = data1v2["avg(U (Magnitude))"]
 velocity2 = data2["avg(U (Magnitude))"]
 velocity3 = data3["avg(U (Magnitude))"]
-velocity4 = data4["avg(U (Magnitude))"]
-velocity5 = data5["avg(U (Magnitude))"]
-velocity6 = data6["avg(U (Magnitude))"]
 
 # Plot the temperature data
 plt.grid(True) 
 time900 = data3["Time"]
-time900NoBaffles = data4["Time"]
 
-#plt.plot(time900, velocity1, label='Ushell = 0.01(m/s), Re = 1000', color='red')
-#plt.plot(time900, velocity2, label='Ushell = 0.03 (m/s), Re = 3000', color='green')
-plt.plot(time900, velocity3, label='Ushell = 0.05 (m/s), Re = 5000', color='blue')
-#plt.plot(time900NoBaffles, velocity4, label='Ushell = 0.01(m/s), Re = 1000, No Baffles', color='yellow')
-#plt.plot(time900, velocity5, label='Ushell = 0.03 (m/s), Re = 3000, No Baffles', color='purple')
-plt.plot(time900, velocity6, label='Ushell = 0.05 (m/s), Re = 5000, No Baffles', color='orange')
-
+plt.plot(time900, velocity3, label='Ushell = 0.005 (m/s), color='black')
+plt.plot(time900, velocity1, label='Ushell = 0.01(m/s), color='red')
+#plt.plot(time900, velocity1v2, label='Ushell = 0.01 (m/s) - No Resistance, color='green')
+plt.plot(time900, velocity2, label='Ushell = 0.079479 (m/s), color='blue')
 
 plt.legend()
 plt.xlabel('Time (s)')
@@ -42,5 +33,6 @@ formatter = ScalarFormatter(useMathText=True)
 formatter.set_scientific(False)
 formatter.set_useOffset(False)
 plt.gca().yaxis.set_major_formatter(formatter)
-plt.savefig('velocityPlotRe1000.png')
+fileName ="VelocityPlotGeneral"
+plt.savefig('/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/plots/' + fileName + '.png')
 plt.show()
