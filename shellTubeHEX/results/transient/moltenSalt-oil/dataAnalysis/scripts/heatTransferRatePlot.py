99b275287d6be96126638bc4c5487ccb2c03f285
HenriqueCCaldas <henrique.c.caldas@tum.de>
2024-07-14 22:02:54 +0200

    Adding new velocity data set for molten salt

--- shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/scripts/heatTransferRatePlot.py
+++ shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/scripts/heatTransferRatePlot.py
@@ -5,39 +5,47 @@ from matplotlib.ticker import ScalarFormatter
 
 # Read the CSV file into a pandas DataFrame
 
-data1 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-01.csv")
-data1v2 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-01NoRes.csv")
-data2 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-079479.csv")
-data3 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-005.csv")
+data1 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-005.csv")
+data2 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-01.csv")
+#data1v2 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-01NoRes.csv")
+data3 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-03.csv")
+data4 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-079479.csv")
 
 # Extract the temperature column from the DataFrame
 temperature1 = data1["avg(T)"]
-temperature1v2 = data1v2["avg(T)"]
+#temperature1v2 = data1v2["avg(T)"]
 temperature2 = data2["avg(T)"]
 temperature3 = data3["avg(T)"]
+temperature4 = data4["avg(T)"]
 
 velocity1 = data1["avg(U (Magnitude))"]
-velocity1v2 = data1v2["avg(U (Magnitude))"]
+#velocity1v2 = data1v2["avg(U (Magnitude))"]
 velocity2 = data2["avg(U (Magnitude))"]
 velocity3 = data3["avg(U (Magnitude))"]
+velocity4 = data4["avg(U (Magnitude))"]
 
 time900 = data1["Time"]
+time900v2 = data3["Time"]
 
-cp = 1396.018 + (0.172 * temperature1)  # J/kg*K
-density = 2263.723 - (0.636 * temperature1)  # kg/m^3
+cp1 = 1396.018 + (0.172 * temperature1)  # J/kg*K
+density1 = 2263.723 - (0.636 * temperature1)  # kg/m^3
+cp2 = 1396.018 + (0.172 * temperature2)  # J/kg*K
+density2 = 2263.723 - (0.636 * temperature2)  # kg/m^3
+cp3 = 1396.018 + (0.172 * temperature3)  # J/kg*K
+density3 = 2263.723 - (0.636 * temperature3)  # kg/m^3
 area = 0.04 * 0.04 * math.pi  # m^2
 initialTemperature =  563.15 # K
 
+Q1 = cp1 * density1 * ((temperature1 - initialTemperature) * velocity1) * area * 1/1000 # W to kW
+Q2 = cp2 * density2 * ((temperature2 - initialTemperature) * velocity2) * area * 1/1000 # W to kW
+Q3 = cp3 * density3 * ((temperature3 - initialTemperature) * velocity3) * area * 1/1000 # W to kW
+#Q4 = cp  * density * ((temperature4 - initialTemperature) * velocity4) * area * 1/1000
 
-Q1 = cp * density * ((temperature1 - initialTemperature) * velocity1) * area * 1/1000 # W to kW
-Q1v2 = cp * density * ((temperature1v2 - initialTemperature) * velocity1v2) * area * 1/1000 # W to kW
-Q2 = cp * density * ((temperature2 - initialTemperature) * velocity2) * area * 1/1000 # W to kW
-Q3 = cp * density * ((temperature3 - initialTemperature) * velocity3) * area * 1/1000 # W to kW
-
-plt.plot(time900, Q3, label='Ushell = 0.005(m/s)', color='red')
-plt.plot(time900, Q1, label='Ushell = 0.01(m/s)', color='blue')
+plt.plot(time900, Q1, label='Ushell = 0.005(m/s)', color='red')
 #plt.plot(time900, Q1v2, label='Ushell = 0.01(m/s) - No Resistance', color='green')
-plt.plot(time900, Q2, label='Ushell = 0.079479(m/s)', color='green')
+plt.plot(time900, Q2, label='Ushell = 0.01(m/s)', color='blue')
+plt.plot(time900v2, Q3, label='Ushell = 0.03(m/s)', color='green')
+#plt.plot(time900, Q4, label='Ushell = 0.079479(m/s)', color='green')
 
 plt.legend()
 plt.xlabel('Time (s)', fontsize = 12)
@@ -48,6 +56,6 @@ plt.yticks(fontsize = 12)
 
 plt.grid(True)
 
-fileName ="heatTransferRatePlotGeneral"
+fileName ="heatTransferRatePlotGeneralUpdated"
 plt.savefig('/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/plots/' + fileName + '.png')
 plt.show()
--- shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/scripts/temperaturePlot.py
+++ shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/scripts/temperaturePlot.py
@@ -1,27 +1,30 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data1 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-01.csv")
-data1v2 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-01NoRes.csv")
-data2 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-079479.csv")
-data3 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-005.csv")
+data1 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-005.csv")
+data2 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-01.csv")
+#data1v2 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-01NoRes.csv")
+data3 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-03.csv")
+data4 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/data/MSOil_0-079479.csv")
+
 # Extract the temperature column from the DataFrame
 temperature1 = data1["avg(T)"]
-temperature1v2 = data1v2["avg(T)"]
+#temperature1v2 = data1v2["avg(T)"]
 temperature2 = data2["avg(T)"]
-temperarture3 = data3["avg(T)"]
+temperature3 = data3["avg(T)"]
 
 # Plot the temperature data
 plt.grid(True) 
 
 time900 = data1["Time"]
+time900v2 = data3["Time"]
 
 # Choose the data to plot (Please select pairs 1-4, 2-5, 3-6)
 
-plt.plot(time900, temperarture3, label='Ushell = 0.005 (m/s)', color='red')
-plt.plot(time900, temperature1, label='Ushell = 0.01(m/s)', color='blue')
-#plt.plot(time900, temperature1v2, label='Ushell = 0.01 (m/s) - No Resistance', color='green')
-plt.plot(time900, temperature2, label='Ushell = 0.079479 (m/s)', color='green')
+plt.plot(time900, temperature1, label='Ushell = 0.005 (m/s)', color='red')
+plt.plot(time900, temperature2, label='Ushell = 0.01(m/s)', color='blue')
+plt.plot(time900v2, temperature3, label='Ushell = 0.03 (m/s)', color='green')
+#plt.plot(time900, temperature4, label='Ushell = 0.079479 (m/s)', color='green')
 
 
 plt.legend()
@@ -30,7 +33,7 @@ plt.ylabel('Temperature (K)',  fontsize = 12)
 plt.title('Outlet Average Temperature - Molten Salt & Thermal Oil',  fontsize = 12)
 plt.xticks(range(0, int(max(time900)) + 100, 100),  fontsize = 12)
 plt.yticks( fontsize = 12)
-fileName ="TemperaturePlotGerenal"
+fileName ="TemperaturePlotGerenalIpdated"
 plt.savefig('/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/moltenSalt-oil/dataAnalysis/plots/' + fileName + '.png')
 plt.show()
 
