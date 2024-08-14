fc4852ac90422cd02e6dc818b6ca507f03097394
HenriqueCCaldas <henrique.c.caldas@tum.de>
2024-08-14 12:49:43 +0200

    Final Commit with last documentation

---
+++ henrique/shellTubeHEX/results/transient/water-water/dataAnalysis/scripts/temperaturePlot.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data1 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/water-water/dataAnalysis/data/baffles/outletResultsBaffleRe1000.csv")
+data2 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/water-water/dataAnalysis/data/baffles/outletResultsBaffleRe3000.csv")
+data3 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/water-water/dataAnalysis/data/baffles/outletResultsBaffleRe5000.csv")
+
+data4 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/water-water/dataAnalysis/data/noBaffles/outletResultsNoBaffleRe1000.csv")
+data5 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/water-water/dataAnalysis/data/noBaffles/outletResultsNoBaffleRe3000.csv")
+data6 = pd.read_csv("/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/water-water/dataAnalysis/data/noBaffles/outletResultsNoBaffleRe5000.csv")
+
+# Extract the temperature column from the DataFrame
+temperature1 = data1["avg(T)"]
+temperature2 = data2["avg(T)"]
+temperature3 = data3["avg(T)"]
+temperature4 = data4["avg(T)"]
+temperature5 = data5["avg(T)"]
+temperature6 = data6["avg(T)"]
+
+# Plot the temperature data
+plt.grid(True) 
+
+time900 = data3["Time"]
+time900NoBaffles = data4["Time"]
+
+# Choose the data to plot (Please select pairs 1-4, 2-5, 3-6)
+
+plt.plot(time900, temperature1, label='uin = 0.01, Baffles', color='red')
+plt.plot(time900NoBaffles, temperature4, label='uin = 0.01, No Baffles', color='red', linestyle='dashed')
+plt.plot(time900, temperature2, label='uin = 0.03, Baffles', color='blue')
+plt.plot(time900, temperature5, label='uin = 0.03, No Baffles', color='blue', linestyle='dashed')
+plt.plot(time900, temperature3, label='uin = 0.05, Baffles', color='green')
+plt.plot(time900, temperature6, label='uin = 0.05, No Baffles', color='green', linestyle='dashed')
+
+
+plt.legend()
+plt.xlabel('Time (s)')
+plt.ylabel('Temperature (K)')
+plt.title('Shell Outlet Average Temperature - Baffles & No Baffles')
+plt.xticks(range(0, int(max(time900)) + 100, 100))
+fileName ="TemperatureGeneral"
+plt.savefig('/home/henrique/OpenFOAM/henrique-11/run/heat-exchanger-simulation/shellTubeHEX/results/transient/water-water/dataAnalysis/plots/test/'+fileName+'.png')
+plt.show()
+
+
--- old/meshes/sim_2D.geo
+++
@@ -1,2 +0,0 @@
-// Gmsh project created on Mon May 20 12:19:51 2024
-SetFactory("OpenCASCADE");
